d7d8f681b392b82b2c3bd5a129e8ff9cb33dbc80
Jan Deinhard <jan.deinhard@sennheiser-connect.com>
2017-10-23 21:36:28 +0200

    add copy constructor and assignement operator for vector

--- include/vector.h
+++ include/vector.h
@@ -9,17 +9,34 @@ template <typename T>
 class vector {
 public:
     typedef T value_type;
+    typedef T* iterator;
+    typedef const T* const_iterator;
     typedef T& reference;
     typedef const T& const_reference;
     typedef T* pointer;
     typedef std::size_t size_type;
 
     vector() : data_(nullptr), capacity_(0), size_(0) {}
+    vector(const vector& other) : data_(nullptr), capacity_(other.capacity_), size_(other.size_) {
+        data_ = new value_type[capacity_];
+        memcpy(data_, other.data_, size_ * sizeof(value_type));
+    }
     virtual ~vector() {
         if (data_) {
             assert(capacity_ > 0);
             delete[] data_;
         }
+    }
+
+    vector& operator=(vector other) {
+        other.swap(*this);
+        return *this;
+    }
+
+    void swap(vector& other) {
+        std::swap(data_, other.data_);
+        std::swap(capacity_, other.capacity_);
+        std::swap(size_, other.size_);
     }
 
     constexpr size_type size() const { return size_; }
@@ -30,7 +47,7 @@ public:
             if (capacity_ != 0) {
                 const size_type new_size = 2 * capacity_;
                 pointer data = new value_type[new_size];
-                memcpy(data, data_, capacity_ * sizeof(value_type));
+                memcpy(data, data_, size_ * sizeof(value_type));
                 delete[] data_;
                 data_ = data;
                 capacity_ = new_size;
@@ -41,6 +58,12 @@ public:
         }
         data_[size_++] = value;
     }
+
+    constexpr iterator begin() noexcept { return data_; }
+    constexpr const_iterator begin() const noexcept { return data_; }
+
+    constexpr iterator end() noexcept { return data_ + size_; }
+    constexpr const_iterator end() const noexcept { return data_ + size_; }
 
 private:
     pointer data_;
--- tests/vector.cpp
+++ tests/vector.cpp
@@ -8,6 +8,50 @@ TEST_CASE("construct a new vector", "[vector]") {
     REQUIRE(v.capacity() == 0);
 }
 
+TEST_CASE("copy construct a new vector", "[vector]") {
+    jade::vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+
+    jade::vector<int> v2(v1);
+
+    REQUIRE(v2.size() == 3);
+    REQUIRE(v2.capacity() == 4);
+
+    int k = 1;
+    for (const auto& i : v2) {
+        REQUIRE(k++ == i);
+    }
+}
+
+TEST_CASE("assignement operator", "[vector]") {
+    jade::vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+
+    REQUIRE(v1.size() == 2);
+    REQUIRE(v1.capacity() == 2);
+
+    jade::vector<int> v2;
+    v2.push_back(3);
+    v2.push_back(4);
+    v2.push_back(5);
+
+    REQUIRE(v2.size() == 3);
+    REQUIRE(v2.capacity() == 4);
+
+    v2 = v1;
+
+    REQUIRE(v2.size() == 2);
+    REQUIRE(v2.capacity() == 2);
+
+    int k = 1;
+    for (const auto& i : v2) {
+        REQUIRE(k++ == i);
+    }
+}
+
 TEST_CASE("capacity doubles everytime the size increases to the current max. capacity", "[vector]") {
     jade::vector<int> v;
     v.push_back(1);
